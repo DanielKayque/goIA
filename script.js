8023607adc46442ec40c06d1f15b269915c35960
Daniel Kayque <daanielkayque@gmail.com>
2025-10-07 12:15:44 -0300

    arrumei o corte do efeito lateral e o tornei 100% responsivo

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ function handleClick(event) {
   headerAtivo.classList.toggle("ativo");
   navMobile.classList.toggle("ativo");
 }
+
+const sections = document.querySelectorAll("section");
+let ticking = false;
+
+function initAnimationOpacity() {
+  sections.forEach((section) => {
+    const top = section.getBoundingClientRect().top;
+    const metade = window.innerHeight * 0.9;
+    if (top < metade) {
+      section.classList.add("ativo");
+    } else {
+      section.classList.remove("ativo");
+    }
+  });
+  ticking = false;
+}
+
+window.addEventListener("scroll", () => {
+  if (!ticking) {
+    window.requestAnimationFrame(initAnimationOpacity);
+    ticking = true;
+  }
+});
+
+initAnimationOpacity();
